d7a91455fdc093ca914f30a7f287898fb79f1249
HenriBrg <hnr.berger@gmail.com>
2020-02-14 11:46:08 +0100

    Update

--- clean.sh
+++ clean.sh
@@ -1,5 +1,13 @@
 kubectl delete --all pods
 kubectl delete --all services
 kubectl delete --all deployments
+kubectl delete --all statefulset.apps
 
+minikube stop
 minikube delete
+
+
+# To check
+
+# $ helm list
+# $ kubectl get all
