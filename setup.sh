6e4eb846ea5592a479d1590da8ab81aadcc2d5ab
Henri Berger <hnr.berger@gmail.com>
2020-05-10 16:48:30 +0200

    Soon done i guess

--- setup.sh
+++ setup.sh
@@ -50,8 +50,8 @@ then
 		echo
 		echo "	START MINIKUBE"
 		echo
-		# minikube start --vm-driver=docker --extra-config=apiserver.service-node-port-range=1-35000
-		minikube start --cpus=2 --disk-size 11000 --vm-driver=virtualbox --extra-config=apiserver.service-node-port-range=1-35000
+		minikube start --vm-driver=docker --extra-config=apiserver.service-node-port-range=1-35000
+		# minikube start --cpus=2 --disk-size 11000 --vm-driver=virtualbox --extra-config=apiserver.service-node-port-range=1-35000
 		minikube addons enable dashboard
 		minikube addons enable ingress
 		minikube addons enable metrics-server
@@ -92,17 +92,17 @@ cp $srcs/telegraf.conf							$srcs/grafana/srcs/telegraf.conf
 
 # ADD PASSWORD CONFIG
 
-sed -i "" s/__SSH_USERNAME__/$SSH_USERNAME/g 	 	$srcs/nginx/srcs/install.sh
-sed -i "" s/__SSH_PASSWORD__/$SSH_PASSWORD/g 	 	$srcs/nginx/srcs/install.sh
-sed -i "" s/__FTPS_USERNAME__/$FTPS_USERNAME/g 	 	$srcs/ftps/srcs/install.sh
-sed -i "" s/__FTPS_PASSWORD__/$FTPS_PASSWORD/g 	 	$srcs/ftps/srcs/install.sh
-sed -i "" s/__MINIKUBE_IP__/$MINIKUBE_IP/g 	 		$srcs/ftps/Dockerfile
-sed -i "" s/__DB_USER__/$DB_USER/g 	 				$srcs/wordpress/srcs/wp-config.php
-sed -i "" s/__DB_PASSWORD__/$DB_PASSWORD/g 	 		$srcs/wordpress/srcs/wp-config.php
-sed -i "" s/__DB_USER__/$DB_USER/g 	 				$srcs/mysql/srcs/start.sh
-sed -i "" s/__DB_PASSWORD__/$DB_PASSWORD/g 	 		$srcs/mysql/srcs/start.sh
-sed -i "" s/__MINIKUBE_IP__/$MINIKUBE_IP/g 	 		$srcs/wordpress/srcs/wordpress.sql
-sed -i "" s/__MINIKUBE_IP__/$MINIKUBE_IP/g 	 		$srcs/nginx/srcs/index.html
+sed -i  s/__SSH_USERNAME__/$SSH_USERNAME/g 	 	$srcs/nginx/srcs/install.sh
+sed -i  s/__SSH_PASSWORD__/$SSH_PASSWORD/g 	 	$srcs/nginx/srcs/install.sh
+sed -i  s/__FTPS_USERNAME__/$FTPS_USERNAME/g 	 	$srcs/ftps/srcs/install.sh
+sed -i  s/__FTPS_PASSWORD__/$FTPS_PASSWORD/g 	 	$srcs/ftps/srcs/install.sh
+sed -i  s/__MINIKUBE_IP__/$MINIKUBE_IP/g 	 		$srcs/ftps/Dockerfile
+sed -i  s/__DB_USER__/$DB_USER/g 	 				$srcs/wordpress/srcs/wp-config.php
+sed -i  s/__DB_PASSWORD__/$DB_PASSWORD/g 	 		$srcs/wordpress/srcs/wp-config.php
+sed -i  s/__DB_USER__/$DB_USER/g 	 				$srcs/mysql/srcs/start.sh
+sed -i  s/__DB_PASSWORD__/$DB_PASSWORD/g 	 		$srcs/mysql/srcs/start.sh
+sed -i  s/__MINIKUBE_IP__/$MINIKUBE_IP/g 	 		$srcs/wordpress/srcs/wordpress.sql
+sed -i  s/__MINIKUBE_IP__/$MINIKUBE_IP/g 	 		$srcs/nginx/srcs/index.html
 
 # <><><><><><><><><><><> BUILD CONTAINER & APPLY CONFIG <><><><><><><><><><><><>
 
@@ -147,7 +147,7 @@ kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql wordpr
 
 for service in "${services[@]}"
 do
-	sed -i "" s/__$service-POD__/$(kubectl get pods | grep $service | cut -d" " -f1)/g $srcs/grafana/srcs/$service.json
+	sed -i s/__$service-POD__/$(kubectl get pods | grep $service | cut -d" " -f1)/g $srcs/grafana/srcs/$service.json
 	kubectl exec -i $(kubectl get pods | grep grafana | cut -d" " -f1) -- /bin/sh -c "cat >> /usr/share/grafana/conf/provisioning/dashboards/$service.json" < $srcs/grafana/srcs/$service.json > /dev/null 2>&1
 done
 
--- tmp.sh
+++
@@ -1,5 +0,0 @@
-SSH_USERNAME=henri
-srcs=./srcs
-
-sed -i '' s/__SSH_USERNAME__/$SSH_USERNAME/g $srcs/file.txt
-sed -i '' s/__SSH_USERNAME__/$SSH_USERNAME/g $srcs/file.txt
