2a7b27b3b879fc9816a491beae4a5896a2d6a330
HenriBrg <hnr.berger@gmail.com>
2020-05-13 14:22:27 +0200

    SSH background

--- srcs/nginx/srcs/loop.sh
+++ srcs/nginx/srcs/loop.sh
@@ -3,16 +3,12 @@ while [ $EXIT_CODE -gt 0 ]
 do
 	ps | grep -v grep | grep sshd
 	EXIT_CODE=$?
-	if [ $EXIT_CODE -eq 0 ]
+	if [ $EXIT_CODE -gt 0 ]
 	then
-		echo "SSH OK"
+		:
 	else
-		echo "SSH KO"
-		/etc/init.d/sshd restart > /dev/null 2>&1
+		/etc/init.d/sshd restart # > /dev/null 2>&1
 	fi
 	sleep 5
-	EXIT_CODE=1 # Par précaution
+	EXIT_CODE=1
 done
-
-# Avec le package openssh, le processus lancé s'appelle "sshd"
-# On recherche le processus sshd en excluant le processus grep de recherche lui même via -v
